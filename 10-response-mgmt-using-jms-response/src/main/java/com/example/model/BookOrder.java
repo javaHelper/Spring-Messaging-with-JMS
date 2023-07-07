e3575ec95f164fa6292a35de1c36cfff21efd398
Prateek <54174687+javaHelper@users.noreply.github.com>
2023-07-07 19:10:06 +0530

    Response management using JmsResponse

---
+++ 10-response-mgmt-using-jms-response/src/main/java/com/example/model/BookOrder.java
@@ -0,0 +1,19 @@
+package com.example.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class BookOrder {
+    private String bookOrderId;
+    private Book book;
+    private Customer customer;
+
+    @JsonCreator
+    public BookOrder(@JsonProperty("bookOrderId") String bookOrderId, @JsonProperty("book") Book book, @JsonProperty("customer") Customer customer) {
+        this.bookOrderId = bookOrderId;
+        this.book = book;
+        this.customer = customer;
+    }
+}
